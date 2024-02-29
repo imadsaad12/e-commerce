ffe33b34ba87af4f54c390852f05b7f8676a7659
Imad Saad <imad.alhaj.saad@gmail.com>
2024-02-29 12:39:21 +0200

    fix regarding cookies

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const categoriesRoutes = require("./routes/categoriesRoutes");
 const cookieParser = require("cookie-parser");
 const corsOptions = {
   credentials: true,
-  origin: "*",
 };
 
 dotenv.config();
